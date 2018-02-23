7d607061cbda14914e497a787bf68054c7dbd3c6
jm.sousa <joao.sousa@surgsci.uu.se>
2018-02-23 12:44:03 +0100

    pack

---
+++ Project/__init__.py
@@ -0,0 +1,5 @@
+import GOL_project as GOL 
+
+
+def Game():
+    return GOL
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Conway_py_proj',
+      version='1.0',
+      description='Conway game of Life',
+      url='https://github.com/JoaoMSousa/class_project',
+      author='Adapted by JMS',
+      license='Free',
+      packages=['Project'],
+      zip_safe=False)
